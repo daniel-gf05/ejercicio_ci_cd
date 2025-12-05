1fbf01ba8f5819ccc765cc0fb7772a6125c17a9f
daniel-gf05 <danielfgayoso@gmail.com>
2025-12-05 09:13:13 +0100

    toupper test

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
 def saludo(nombre):
     return f'Hola {nombre}'
+
+def toUpper(text):
+    return text.upper()
--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,4 +1,7 @@
-from src.main import saludo
+from src.main import saludo, toUpper
 
 def test_main():
     assert 'Hola Daniel' in saludo('Daniel')
+    
+def test_toUpper():
+    assert toUpper('daniel') == 'DANIEL'
